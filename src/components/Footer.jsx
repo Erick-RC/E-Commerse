6622ad023989914c5936ba9c8c555ad690cf66af
ErickRabago <164416841+ErickRabago@users.noreply.github.com>
2024-05-30 08:54:42 -0700

    first

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+export function Footer () {
+  // const { filters } = useFilters()
+
+  return (
+    <footer className='footer'>
+      <h5>Shopping Cart</h5>
+    </footer>
+  )
+}
